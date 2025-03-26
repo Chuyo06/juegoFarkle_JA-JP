e6d879e666150c03a515db5120ab8a5480a94d46
jesus alejandro manjarrez torres <jalex_manjarrez@outlook.com>
2025-03-26 12:05:41 -0700

    Prueba de conexion a Github

---
+++ src/main/java/com/mycompany/juego/Juego.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juego;
+
+/**
+ *
+ * @author jalex
+ */
+public class Juego {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
