6871fc55df02666f255ae2c2d40c8f477d875b3b
jesus alejandro manjarrez torres <jalex_manjarrez@outlook.com>
2025-03-26 12:10:03 -0700

    PruebaSSSSS

--- src/main/java/com/mycompany/juego/Juego.java
+++ src/main/java/com/mycompany/juego/Juego.java
@@ -12,5 +12,8 @@ public class Juego {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("PRUEBA");
+    System.out.println("DE COMMIT");
+    System.out.println("Y PUSH!");
     }
 }
