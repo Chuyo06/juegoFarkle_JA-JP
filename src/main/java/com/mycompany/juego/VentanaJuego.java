e9f4f3b6ed999983c81fe8befd96f7c79a9fa82d
jesus alejandro manjarrez torres <jalex_manjarrez@outlook.com>
2025-04-07 19:35:47 -0700

    +Cambios minimos en imagenes y se aniaderon Labels

--- src/main/java/com/mycompany/juego/VentanaJuego.java
+++ src/main/java/com/mycompany/juego/VentanaJuego.java
@@ -4,7 +4,9 @@
  */
 package com.mycompany.juego;
 
+import java.awt.Color;
 import java.awt.Image;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -18,13 +20,13 @@ public class VentanaJuego extends javax.swing.JFrame {
 
     private Farkle juego;
     private VentanaInicio ventanaInicio ;
-    
+    private List<Integer> valoresDadosSeleccionados = new ArrayList<>(); //Aqui se guardan los valores de los dados que se seleccionaron.
     //Constructor recibe la 
     public VentanaJuego(VentanaInicio ventanaInicio) {
         initComponents();
-        System.out.println("VentanaJuego se ha inicializado");  // Debugging line
+  
          this.ventanaInicio = ventanaInicio;  // Asignar la instancia recibida
-         
+         getContentPane().setBackground(new Color(237, 255, 250)); // color de fondo
         juego = new Farkle(ventanaInicio.getJugadores());
         
         redimensionarImgPuntos();
@@ -67,38 +69,38 @@ public class VentanaJuego extends javax.swing.JFrame {
   
   private void redimensionarImgDados()
   {
-          ImageIcon dado1Icon = new ImageIcon(getClass().getResource("/dados/dice_1.png"));
-          dado1.setSize(80, 80); //  tamaño que quieras
+          ImageIcon dado1Icon = new ImageIcon(getClass().getResource("/logo/signo.png"));
+          dado1.setSize(70, 70); //  tamaño que quieras
           Image imagenEscalada1 = dado1Icon.getImage().getScaledInstance(
           dado1.getWidth(), dado1.getHeight(), Image.SCALE_SMOOTH);
           dado1.setIcon(new ImageIcon(imagenEscalada1));
 
-          ImageIcon dado2Icon = new ImageIcon(getClass().getResource("/dados/dice_2.png"));
-          dado2.setSize(80, 80); //  tamaño que quieras
+          ImageIcon dado2Icon = new ImageIcon(getClass().getResource("/logo/signo.png"));
+          dado2.setSize(70, 70); //  tamaño que quieras
           Image imagenEscalada2 = dado2Icon.getImage().getScaledInstance(
           dado2.getWidth(), dado2.getHeight(), Image.SCALE_SMOOTH);
           dado2.setIcon(new ImageIcon(imagenEscalada2));
           
-          ImageIcon dado3Icon = new ImageIcon(getClass().getResource("/dados/dice_3.png"));
-          dado3.setSize(80, 80); //  tamaño que quieras
+          ImageIcon dado3Icon = new ImageIcon(getClass().getResource("/logo/signo.png"));
+          dado3.setSize(70, 70); //  tamaño que quieras
           Image imagenEscalada3 = dado3Icon.getImage().getScaledInstance(
           dado3.getWidth(), dado3.getHeight(), Image.SCALE_SMOOTH);
           dado3.setIcon(new ImageIcon(imagenEscalada3));
           
-          ImageIcon dado4Icon = new ImageIcon(getClass().getResource("/dados/dice_4.png"));
-          dado4.setSize(80, 80); //  tamaño que quieras
+          ImageIcon dado4Icon = new ImageIcon(getClass().getResource("/logo/signo.png"));
+          dado4.setSize(70, 70); //  tamaño que quieras
           Image imagenEscalada4 = dado4Icon.getImage().getScaledInstance(
           dado4.getWidth(), dado4.getHeight(), Image.SCALE_SMOOTH);
           dado4.setIcon(new ImageIcon(imagenEscalada4));
           
-          ImageIcon dado5Icon = new ImageIcon(getClass().getResource("/dados/dice_5.png"));
-          dado5.setSize(80, 80); //  tamaño que quieras
+          ImageIcon dado5Icon = new ImageIcon(getClass().getResource("/logo/signo.png"));
+          dado5.setSize(70, 70); //  tamaño que quieras
           Image imagenEscalada5 = dado4Icon.getImage().getScaledInstance(
           dado5.getWidth(), dado5.getHeight(), Image.SCALE_SMOOTH);
           dado5.setIcon(new ImageIcon(imagenEscalada5));
           
-          ImageIcon dado6Icon = new ImageIcon(getClass().getResource("/dados/dice_6.png"));
-          dado6.setSize(80, 80); //  tamaño que quieras
+          ImageIcon dado6Icon = new ImageIcon(getClass().getResource("/logo/signo.png"));
+          dado6.setSize(70, 70); //  tamaño que quieras
           Image imagenEscalada6 = dado6Icon.getImage().getScaledInstance(
           dado6.getWidth(), dado6.getHeight(), Image.SCALE_SMOOTH);
           dado6.setIcon(new ImageIcon(imagenEscalada6));
@@ -120,6 +122,11 @@ public class VentanaJuego extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         puntos = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(900, 510));
@@ -157,12 +164,24 @@ public class VentanaJuego extends javax.swing.JFrame {
         puntos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/logo/puntos.png"))); // NOI18N
         puntos.setPreferredSize(new java.awt.Dimension(300, 400));
 
+        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jButton1.setText("Lanzar dados");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+
+        jButton2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jButton2.setText("Sumar Puntos");
+
+        jLabel1.setText("jLabel1");
+
+        jLabel2.setText("jLabel2");
+
+        jLabel3.setText("jLabel3");
+
+        jLabel4.setText("jLabel4");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -177,27 +196,44 @@ public class VentanaJuego extends javax.swing.JFrame {
                         .addGap(75, 75, 75)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(dado1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(106, 106, 106))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(dado4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(68, 68, 68)))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(dado1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(106, 106, 106))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addComponent(dado4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(68, 68, 68)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(dado2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(108, 108, 108)
+                                        .addComponent(dado3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(dado5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(61, 61, 61)
+                                        .addComponent(dado6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(dado2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(108, 108, 108)
-                                .addComponent(dado3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(dado5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(61, 61, 61)
-                                .addComponent(dado6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
-                .addComponent(puntos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(74, 74, 74))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(125, 125, 125)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(38, 38, 38)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                        .addComponent(puntos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(74, 74, 74))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(64, 64, 64)
+                        .addComponent(jLabel2)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(133, 133, 133)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel3))
+                .addGap(130, 130, 130))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -219,9 +255,23 @@ public class VentanaJuego extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(33, 33, 33)
                         .addComponent(puntos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(23, 23, 23))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel1))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(31, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel4))
+                        .addGap(22, 22, 22))))
         );
 
         pack();
@@ -229,11 +279,11 @@ public class VentanaJuego extends javax.swing.JFrame {
 
     private void clickDado1(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_clickDado1
         
+        
     }//GEN-LAST:event_clickDado1
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 
-    
         juego.lanzarDados();//Se lanzan los dados y en el metodo lanzarDados() se guardan los valores.
         actualizarImagenesDados(); //Aqui las imagenes cambian cuando se presiona el boton
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -247,6 +297,11 @@ public class VentanaJuego extends javax.swing.JFrame {
     private javax.swing.JLabel dado5;
     private javax.swing.JLabel dado6;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel puntos;
     // End of variables declaration//GEN-END:variables
